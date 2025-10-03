1aa0dd65289e69d0ecf35e2769d0ebaa1b2be002
Vinay Negi <vinegi004@gmail.com>
2025-10-03 18:19:29 +0530

    Layout changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,13 +15,13 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DefaultTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#000000" style="dark" />
     </ThemeProvider>
   );
 }
