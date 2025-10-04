bf3bfe2c2c5ac0034f00ac8eabb9fa5ac2e8aed1
Vinay Negi <vinegi004@gmail.com>
2025-10-04 09:28:34 +0530

    Navigation bar changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,7 +32,7 @@ export default function TabLayout() {
         options={{
           title: "History",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="clock.fill" color={color} />
+            <IconSymbol size={28} name="clock" color={color} />
           ),
         }}
       />
