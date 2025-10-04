92816ebc43ec5f162ca23d454e00a834451c15f3
Vinay Negi <vinegi004@gmail.com>
2025-10-04 09:19:51 +0530

    Add dummy sprint board

---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const history = () => {
+  return (
+    <View>
+      <Text>history</Text>
+    </View>
+  )
+}
+
+export default history
