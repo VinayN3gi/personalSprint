cd7dc7b84fa3f9ac5432fa2527dff8d94dce9268
Vinay Negi <vinegi004@gmail.com>
2025-10-08 19:47:33 +0530

    Modfiied main layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="signin" />
-        <Stack.Screen name="otp" />
+      <Stack.Screen name="otp" />
     </Stack>
   );
 }
